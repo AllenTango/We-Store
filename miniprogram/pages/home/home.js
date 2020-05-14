be713f6279bc201c0cbb676d1bf2e4d340572fec
Allen <allentango@163.com>
2020-05-15 03:06:37 +0800

    使用腾讯云 数据库
    图片加载 需转换地址

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,66 +1,88 @@
 // pages/home/home.js
-Page({
+const db = wx.cloud.database({
+  env: "dev-pupws",
+});
 
+Page({
   /**
    * Page initial data
    */
   data: {
-
+    productList: [],
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    this.getProductList();
+  },
+  getProductList() {
+    wx.showLoading({
+      title: "正在加载...",
+    });
+    db.collection("product")
+      .get()
+      .then((res) => {
+        console.log(res);
+        wx.hideLoading();
+        const productList = res.data;
+        productList.forEach((product) => {
+          product.price = parseFloat(
+            Math.round(product.price * 100) / 100
+          ).toFixed(2);
+          wx.cloud
+            .getTempFileURL({ fileList: [product.image] })
+            .then(({ fileList }) => {
+              product.image = fileList[0].tempFileURL;
+            })
+            .catch((err) => console.log(err));
+        });
 
+        if (productList.length) {
+          this.setData({
+            productList,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        wx.hideLoading();
+      });
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
